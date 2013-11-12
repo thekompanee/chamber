acee09a8af89d92a40259a1ad1d43a2f0bf9b883
Mark McEahern <mark@mceahern.com>
2013-11-12 08:48:31 -0600

    Keep a list of sources
    Add load! and reload!

--- lib/chamber.rb
+++ lib/chamber.rb
@@ -9,6 +9,45 @@ module Chamber
   def source(filename, options={})
     assert_valid_keys(options)
 
+    add_source(filename, options)
+  end
+
+  def load!
+    sources.each do |source|
+      filename, options = source
+
+      load_source!(filename, options)
+    end
+  end
+
+  def reload!
+    @instance = nil
+    load!
+  end
+
+  def instance
+    @instance ||= Hashie::Mash.new
+  end
+
+private
+
+  def assert_valid_keys(options)
+    unknown_keys = options.keys - [:namespace, :override_from_environment]
+
+    raise(ChamberInvalidOptionError, options) unless unknown_keys.empty?
+  end
+
+  def sources
+    @sources ||= []
+  end
+
+  def add_source(filename, options)
+    sources << [filename, options]
+  end
+
+  def load_source!(filename, options)
+    return unless File.exists?(filename)
+
     hash = hash_from_source(filename, options[:namespace])
     if options[:override_from_environment]
       override_from_environment!(hash)
@@ -17,15 +56,12 @@ module Chamber
     instance.deep_merge!(hash)
   end
 
-  def instance
-    @instance ||= Hashie::Mash.new
-  end
+  def hash_from_source(filename, namespace)
+    contents = open(filename).read
+    hash = YAML.load(ERB.new(contents).result).to_hash
+    hash = Hashie::Mash.new(hash)
 
-private
-  def assert_valid_keys(options)
-    unknown_keys = options.keys - [:namespace, :override_from_environment]
-
-    raise(ChamberInvalidOptionError, options) unless unknown_keys.empty?
+    namespace ? hash[namespace] : hash
   end
 
   def override_from_environment!(hash)
@@ -38,13 +74,5 @@ private
         override_from_environment!(value)
       end
     end
-  end
-
-  def hash_from_source(filename, namespace)
-    contents = open(filename).read
-    hash = YAML.load(ERB.new(contents).result).to_hash
-    hash = Hashie::Mash.new(hash)
-
-    namespace ? hash[namespace] : hash
   end
 end
