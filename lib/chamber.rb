3d159b9c4a5603be6242f5e4ec4087df4614cb03
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-25 14:31:02 -0600

    Chamber can load itself from any path that's passed in to it

--- lib/chamber.rb
+++ lib/chamber.rb
@@ -1,90 +1,16 @@
-require 'erb'
-require 'hashie'
-require 'yaml'
-
-require 'chamber/version'
-
 module Chamber
-  class ChamberInvalidOptionError < ArgumentError; end
 
-  def source(filename, options={})
-    assert_valid_keys(options)
-
-    add_source(filename, options)
+  class << self
+    attr_accessor :basepath
   end
 
-  def load!
-    sources.each do |source|
-      filename, options = source
-
-      load_source!(filename, options)
-    end
+  def self.load(options = {})
+    self.basepath = options.fetch(:basepath)
   end
 
-  def clear!
-    @chamber_instance = nil
-    @chamber_sources = nil
-  end
+  private
 
-  def reload!
-    @chamber_instance = nil
-    load!
-  end
-
-  def instance
-    @chamber_instance ||= Hashie::Mash.new
-  end
-
-  def method_missing(method_name, *args, &block)
-    instance.public_send(method_name, *args, &block)
-  end
-
-  alias_method :env, :instance
-
-private
-
-  def assert_valid_keys(options)
-    unknown_keys = options.keys - [:namespace, :override_from_environment]
-
-    raise(ChamberInvalidOptionError, options) unless unknown_keys.empty?
-  end
-
-  def sources
-    @chamber_sources ||= []
-  end
-
-  def add_source(filename, options)
-    sources << [filename, options]
-  end
-
-  def load_source!(filename, options)
-    return unless File.exists?(filename)
-
-    hash = hash_from_source(filename, options[:namespace])
-    if options[:override_from_environment]
-      override_from_environment!(hash)
-    end
-
-    instance.deep_merge!(hash)
-  end
-
-  def hash_from_source(filename, namespace)
-    contents = open(filename).read
-    hash = YAML.load(ERB.new(contents).result).to_hash || {}
-    hash = Hashie::Mash.new(hash)
-
-    namespace ? hash.fetch(namespace) : hash
-  end
-
-  def override_from_environment!(hash)
-    hash.each_pair do |key, value|
-      next unless value.is_a?(Hash)
-
-      if value.environment
-        hash[key] = ENV[value.environment]
-      else
-        override_from_environment!(value)
-      end
-    end
+  def self.basepath=(pathlike)
+    @basepath = Pathname.new(File.expand_path(pathlike))
   end
 end
