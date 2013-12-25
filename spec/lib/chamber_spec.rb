7701571a2c0508cc0173b218fee32d4d2b40775d
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-25 15:05:32 -0600

    Chamber loads the settings.yml file from the base path by default

--- spec/lib/chamber_spec.rb
+++ spec/lib/chamber_spec.rb
@@ -13,4 +13,14 @@ describe Chamber do
 
     expect(Chamber.basepath.to_s).to eql '/tmp'
   end
+
+  it 'loads settings from a settings.yml file' do
+    allow(File).to receive(:read).
+                   and_return 'settings.yml'
+
+    Chamber.load(:basepath => '/tmp')
+
+    expect(File).to have_received(:read).
+                    with('/tmp/settings.yml')
+  end
 end
