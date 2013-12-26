17d830b88b26472d83b48449f5a6182b635a8b15
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-26 13:27:27 -0600

    Allow the environment to override values in the settings files

--- spec/lib/chamber_spec.rb
+++ spec/lib/chamber_spec.rb
@@ -6,6 +6,15 @@ File.open('/tmp/settings.yml', 'w+') do |file|
 test:
   my_setting: my_value
   my_dynamic_setting: <%= 1 + 1 %>
+  another_level:
+    setting_one: 1
+    setting_two: 2
+    level_three:
+      an_array:
+        - item 1
+        - item 2
+        - item 3
+      a_scalar: 'hello'
   HEREDOC
 end
 
@@ -48,5 +57,18 @@ describe Chamber do
 
   it 'can access the instance via "env"' do
     expect(Chamber.instance.test.my_setting).to eql 'my_value'
+  end
+
+  it 'prefers values stored in environment variables over those in the YAML files' do
+    ENV['TEST_MY_SETTING'] = 'some_other_value'
+    ENV['TEST_ANOTHER_LEVEL_LEVEL_THREE_AN_ARRAY'] = 'something'
+
+    Chamber.load(:basepath => '/tmp')
+    expect(Chamber.instance.test.my_setting).to eql 'some_other_value'
+    expect(Chamber.instance.test.another_level.level_three.an_array).to eql 'something'
+    expect(Chamber.instance.test.my_dynamic_setting).to eql 2
+
+    ENV.delete 'TEST_MY_SETTING'
+    ENV.delete 'TEST_ANOTHER_LEVEL_LEVEL_THREE_AN_ARRAY'
   end
 end
