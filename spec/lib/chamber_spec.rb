cb3b0c42971b87d2fb8e6ec9dd017dddcc1d641f
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-26 23:25:26 -0600

    If the namespaces DSL is called multiple times, it clears the namespaces each time

--- spec/lib/chamber_spec.rb
+++ spec/lib/chamber_spec.rb
@@ -93,4 +93,11 @@ describe Chamber do
     expect(CustomSettings.env.other.everything).to eql 'works'
     expect(Chamber.instance.test.my_dynamic_setting).to eql 2
   end
+
+  it 'does not load multiple namespaces if it is called twice' do
+    Chamber.namespaces :first_namespace_call
+    Chamber.namespaces :second_namespace_call
+
+    expect(Chamber.instance.namespaces).to eql [:second_namespace_call]
+  end
 end
