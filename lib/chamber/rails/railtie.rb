01e5105033f2d8e8f3f7f51b0806201318670ebb
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-28 03:52:36 -0600

    Add a Railtie which loads Chamber with the proper Rails config path and load the rake task

---
+++ lib/chamber/rails/railtie.rb
@@ -0,0 +1,13 @@
+module  Chamber
+module  Rails
+class   Railtie < ::Rails::Railtie
+  initializer 'chamber.load', before: :load_environment_config do
+    Chamber.load(:basepath => Rails.root.join('config'))
+  end
+
+  rake_tasks do
+    load 'chamber/tasks/heroku.rake'
+  end
+end
+end
+end
