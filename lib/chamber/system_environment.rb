d0348d71553ad13106a3f0de8b862e6f6be39268
Jeff Felchner <accounts+git@thekompanee.com>
2014-01-03 22:57:33 -0600

    Add documentation and specs for the SystemEnvironment class

--- lib/chamber/system_environment.rb
+++ lib/chamber/system_environment.rb
@@ -1,7 +1,56 @@
 require 'hashie/mash'
 
+###
+# Internal: Gives access to the existing environment for importing/exporting
+# values.
+#
 class   Chamber
 module  SystemEnvironment
+
+  ###
+  # Internal: Allows the existing environment to be injected into the passed in
+  # hash.  The hash that is passed in is *not* modified, instead a new hash is
+  # returned.
+  #
+  # Examples:
+  #
+  #   ###
+  #   # Injects the current environment variables
+  #   #
+  #   ENV['LEVEL_ONE_1_LEVEL_TWO_1']               = 'env value 1'
+  #   ENV['LEVEL_ONE_1_LEVEL_TWO_2_LEVEL_THREE_1'] = 'env value 2'
+  #
+  #   SystemEnvironment.inject_into(
+  #     level_one_1: {
+  #       level_two_1: 'value 1',
+  #       level_two_2: {
+  #         level_three_1: 'value 2' } } )
+  #
+  #   # => {
+  #     'level_one_1' => {
+  #       'level_two_1' => 'env value 1',
+  #       'level_two_2' => {
+  #         'level_three_1' => 'env value 2',
+  #   }
+  #
+  #   ###
+  #   # Can inject environment variables if said variables are prefixed
+  #   #
+  #   ENV['PREFIX_LEVEL_TWO_1'] = 'env value 1'
+  #   ENV['PREFIX_LEVEL_TWO_2'] = 'env value 2'
+  #
+  #   SystemEnvironment.inject_into({
+  #                                   level_two_1: 'value 1',
+  #                                   level_two_2: 'value 2'
+  #                                 },
+  #                                 ['prefix'])
+  #
+  #   # => {
+  #     'level_two_1' => 'env value 1',
+  #     'level_two_2' => 'env value 2',
+  #   }
+  #
+  #
   def self.inject_into(settings = {}, parent_keys = [])
     with_environment(settings, parent_keys,
       ->(key, value, environment_keys) do
@@ -12,6 +61,40 @@ module  SystemEnvironment
       end)
   end
 
+  ###
+  # Internal: Allows the environment variable-compatible variables to be
+  # extracted from a passed in hash.
+  #
+  # Examples:
+  #
+  #   ###
+  #   # Extracts the environment variables based on the hash keys
+  #   #
+  #   SystemEnvironment.extract_from(
+  #     level_one_1: {
+  #       level_two_1: 'value 1',
+  #       level_two_2: {
+  #         level_three_1: 'value 2' } } )
+  #
+  #   # => {
+  #     'LEVEL_ONE_1_LEVEL_TWO_1'               => 'env value 1',
+  #     'LEVEL_ONE_1_LEVEL_TWO_2_LEVEL_THREE_1' => 'env value 2',
+  #   }
+  #
+  #   ###
+  #   # Can extract environment variables if said variables are prefixed
+  #   #
+  #   SystemEnvironment.extract_from({
+  #                                   level_two_1: 'value 1',
+  #                                   level_two_2: 'value 2'
+  #                                 },
+  #                                 ['prefix'])
+  #
+  #   # => {
+  #     'PREFIX_LEVEL_TWO_1' => 'value 1',
+  #     'PREFIX_LEVEL_TWO_2' => 'value 2',
+  #   }
+  #
   def self.extract_from(settings, parent_keys = [])
     with_environment(settings, parent_keys,
       ->(key, value, environment_keys) do
