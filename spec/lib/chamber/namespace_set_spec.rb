bfdb50b5d81fb95c6ab812865491c83b655b7832
Jeff Felchner <accounts+git@thekompanee.com>
2014-01-08 19:18:44 -0600

    Use our new sweet square bracket NamespaceSet creation in the specs

--- spec/lib/chamber/namespace_set_spec.rb
+++ spec/lib/chamber/namespace_set_spec.rb
@@ -31,14 +31,14 @@ describe  NamespaceSet do
   end
 
   it 'can turn itself into an array' do
-    namespace_set = NamespaceSet.new([:development, 'my host'])
+    namespace_set = NamespaceSet[:development, 'my host']
 
     expect(namespace_set.to_ary).to eq ['development', 'my host']
     expect(namespace_set.to_a).to   eq ['development', 'my host']
   end
 
   it 'can combine itself with an array' do
-    namespace_set = NamespaceSet.new([:development, 'my host'])
+    namespace_set = NamespaceSet[:development, 'my host']
     other_set     = Set['other value', 3]
     combined_set  = namespace_set + other_set
 
@@ -46,7 +46,7 @@ describe  NamespaceSet do
   end
 
   it 'does not modify the set in place if combining with another array' do
-    namespace_set = NamespaceSet.new([:development, 'my host'])
+    namespace_set = NamespaceSet[:development, 'my host']
     other_set     = Set['other value', 3]
     combined_set  = namespace_set + other_set
 
@@ -54,7 +54,7 @@ describe  NamespaceSet do
   end
 
   it 'can combine itself with something that can be converted to an array' do
-    namespace_set = NamespaceSet.new([:development, 'my host'])
+    namespace_set = NamespaceSet[:development, 'my host']
     other_set     = (1..3)
     combined_set  = namespace_set + other_set
 
@@ -62,24 +62,24 @@ describe  NamespaceSet do
   end
 
   it 'does not allow duplicate items' do
-    namespace_set = NamespaceSet.new([:development, :development])
+    namespace_set = NamespaceSet[:development, :development]
 
     expect(namespace_set).to eq ['development']
   end
 
   it 'will process a value by executing it if it is a callable' do
-    namespace_set = NamespaceSet.new([ -> { 'callable' } ])
+    namespace_set = NamespaceSet[ -> { 'callable' } ]
 
     expect(namespace_set).to eq ['callable']
 
-    namespace_set = NamespaceSet.new({ :my_namespace => -> { 'callable' } })
+    namespace_set = NamespaceSet.new( :my_namespace => -> { 'callable' } )
 
     expect(namespace_set).to eq ['callable']
   end
 
   it 'can compare itself to another NamespaceSet' do
-    namespace_set       = NamespaceSet.new([:development, :development])
-    other_namespace_set = NamespaceSet.new([:development, :development])
+    namespace_set       = NamespaceSet[:development, :development]
+    other_namespace_set = NamespaceSet[:development, :development]
 
     expect(namespace_set).to eql other_namespace_set
   end
