51d8cb97c241abaf78a128aaebe1cc2ba7385b63
Jeff Felchner <accounts+git@thekompanee.com>
2014-01-10 00:31:00 -0600

    Introduce the Instance which encapsulates the Configuration and the FileSet

--- lib/chamber.rb
+++ lib/chamber.rb
@@ -1,38 +1,48 @@
-require 'chamber/configuration'
+require 'chamber/instance'
 require 'chamber/rails'
 
 module  Chamber
-  def self.load(options = {})
-    self.config = Configuration.new(options)
+  extend self
+
+  def load(options = {})
+    self.instance = Instance.new(options)
   end
 
-  def self.filenames
-    config.files.filenames
+  def filenames
+    instance.files.filenames
   end
 
-  def self.to_s
-    config.to_s
+  def to_s
+    instance.settings.to_s
   end
 
-  def self.env
-    config.settings
+  def env
+    instance.settings
   end
 
-class << self
-  attr_reader   :config
+  def config
+    instance.configuration
+  end
 
   protected
 
-  attr_writer   :config
+  def instance
+    @@instance ||= Instance.new({})
+  end
+
+  def instance=(new_instance)
+    @@instance = new_instance
+  end
+
+  public
 
   def method_missing(name, *args)
-    return config.settings.public_send(name, *args) if config.settings.respond_to?(name)
+    return instance.settings.public_send(name, *args) if instance.settings.respond_to?(name)
 
     super
   end
 
   def respond_to_missing?(name, include_private = false)
-    config.settings.respond_to?(name, include_private)
+    instance.settings.respond_to?(name, include_private)
   end
-end
 end
