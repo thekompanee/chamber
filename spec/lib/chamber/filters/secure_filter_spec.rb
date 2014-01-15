48f1ca0de04b299942a0594c30c9172e76b465da
Jeff Felchner <accounts+git@thekompanee.com>
2014-01-14 20:57:36 -0600

    Add a SecureFilter for extracting only items marked as 'secure' from a Settings instance

---
+++ spec/lib/chamber/filters/secure_filter_spec.rb
@@ -0,0 +1,36 @@
+require 'rspectacular'
+require 'chamber/filters/secure_filter'
+
+module    Chamber
+module    Filters
+describe  SecureFilter do
+  it 'will return values which are marked as "secure"' do
+    filtered_settings = SecureFilter.execute( data: {
+                                                _secure_my_secure_setting: 'hello' })
+
+    expect(filtered_settings._secure_my_secure_setting).to match 'hello'
+  end
+
+  it 'will not return values which are not marked as "secure"' do
+    filtered_settings = SecureFilter.execute( data: {
+                                                my_secure_setting: 'hello' })
+
+    expect(filtered_settings.my_secure_setting).to be_nil
+  end
+
+  it 'will properly return values even if they are mixed and deeply nested' do
+    filtered_settings = SecureFilter.execute( data: {
+                                                _secure_setting: 'hello',
+                                                secure_setting:  'goodbye',
+                                                secure_group: {
+                                                  _secure_nested_setting:  'movie',
+                                                  insecure_nested_setting: 'dinner' }})
+
+    expect(filtered_settings._secure_setting).to                      eql 'hello'
+    expect(filtered_settings.secure_setting).to                       be_nil
+    expect(filtered_settings.secure_group._secure_nested_setting).to  eql 'movie'
+    expect(filtered_settings.secure_group.insecure_nested_setting).to be_nil
+  end
+end
+end
+end
