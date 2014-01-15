a3654c6b294ff460f520cc97c9f997b9e19837e7
Jeff Felchner <accounts+git@thekompanee.com>
2014-01-14 20:57:36 -0600

    Add a SecureFilter for extracting only items marked as 'secure' from a Settings instance

---
+++ lib/chamber/filters/secure_filter.rb
@@ -0,0 +1,39 @@
+require 'hashie/mash'
+
+module  Chamber
+module  Filters
+class   SecureFilter
+  SECURE_KEY_TOKEN = %r{\A_secure_}
+
+  def initialize(options = {})
+    self.data = Hashie::Mash.new(options.fetch(:data))
+  end
+
+  def self.execute(options = {})
+    self.new(options).send(:execute)
+  end
+
+  protected
+
+  attr_accessor :data
+
+  def execute(raw_data = data)
+    settings = Hashie::Mash.new
+
+    raw_data.each_pair do |key, value|
+      secure_value  = if value.respond_to? :each_pair
+                        execute(value)
+                      elsif key.respond_to? :match
+                        if key.match(SECURE_KEY_TOKEN)
+                          value
+                        end
+                      end
+
+      settings[key] = secure_value unless secure_value.nil?
+    end
+
+    settings
+  end
+end
+end
+end
