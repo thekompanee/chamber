20c7542301bac850a3a4acdc4ab29137378ff86a
Jeff Felchner <accounts+git@thekompanee.com>
2014-06-28 22:59:35 -0500

    Feature: Add InsecureFilter
    This filter is responsible for finding any settings which are supposed to be
    secure and filtering only those which have not been encrypted.

---
+++ spec/lib/chamber/filters/insecure_filter_spec.rb
@@ -0,0 +1,54 @@
+require 'rspectacular'
+require 'chamber/filters/insecure_filter'
+
+module    Chamber
+module    Filters
+describe  InsecureFilter do
+  it 'will return values which are marked as "secure" if they are unencrypted' do
+    filtered_settings = InsecureFilter.execute( data: {
+                                                  _secure_my_secure_setting: 'hello' })
+
+    expect(filtered_settings._secure_my_secure_setting).to match 'hello'
+  end
+
+  it 'will not return values which are not marked as "secure"' do
+    filtered_settings = InsecureFilter.execute( data: {
+                                                  my_secure_setting: 'hello' })
+
+    expect(filtered_settings.my_secure_setting).to be_nil
+  end
+
+  it 'will properly return values even if they are mixed and deeply nested' do
+    filtered_settings = InsecureFilter.execute( data: {
+                                                  _secure_setting: 'hello',
+                                                  secure_setting:  'goodbye',
+                                                  secure_group: {
+                                                    _secure_nested_setting:  'movie',
+                                                    insecure_nested_setting: 'dinner' }})
+
+    expect(filtered_settings._secure_setting).to                      eql 'hello'
+    expect(filtered_settings.secure_setting).to                       be_nil
+    expect(filtered_settings.secure_group._secure_nested_setting).to  eql 'movie'
+    expect(filtered_settings.secure_group.insecure_nested_setting).to be_nil
+  end
+
+  it 'will not return values which are encrypted' do
+    filtered_settings = InsecureFilter.execute( data: {
+                                                  _secure_setting:       'cJbFe0NI5wknmsp2fVgpC/YeBD2pvcdVD+p0pUdnMoYThaV4mpsspg/ZTBtmjx7kMwcF6cjXFLDVw3FxptTHwzJUd4akun6EZ57m+QzCMJYnfY95gB2/emEAQLSz4/YwsE4LDGydkEjY1ZprfXznf+rU31YGDJUTf34ESz7fsQGSc9DjkBb9ao8Mv4cI7pCXkQZDwS5kLAZDf6agy1GzeL71Z8lrmQzk8QQuf/1kQzxsWVlzpKNXWS7u2CJ0sN5eINMngJBfv5ZFrZgfXc86wdgUKc8aaoX8OQA1kKTcdgbE9NcAhNr1+WfNxMnz84XzmUp2Y0H1jPgGkBKQJKArfQ==',
+                                                  secure_setting:        'cJbFe0NI5wknmsp2fVgpC/YeBD2pvcdVD+p0pUdnMoYThaV4mpsspg/ZTBtmjx7kMwcF6cjXFLDVw3FxptTHwzJUd4akun6EZ57m+QzCMJYnfY95gB2/emEAQLSz4/YwsE4LDGydkEjY1ZprfXznf+rU31YGDJUTf34ESz7fsQGSc9DjkBb9ao8Mv4cI7pCXkQZDwS5kLAZDf6agy1GzeL71Z8lrmQzk8QQuf/1kQzxsWVlzpKNXWS7u2CJ0sN5eINMngJBfv5ZFrZgfXc86wdgUKc8aaoX8OQA1kKTcdgbE9NcAhNr1+WfNxMnz84XzmUp2Y0H1jPgGkBKQJKArfQ==',
+                                                  _secure_other_setting: 'hello',
+                                                  secure_group: {
+                                                    _secure_nested_setting:       'cJbFe0NI5wknmsp2fVgpC/YeBD2pvcdVD+p0pUdnMoYThaV4mpsspg/ZTBtmjx7kMwcF6cjXFLDVw3FxptTHwzJUd4akun6EZ57m+QzCMJYnfY95gB2/emEAQLSz4/YwsE4LDGydkEjY1ZprfXznf+rU31YGDJUTf34ESz7fsQGSc9DjkBb9ao8Mv4cI7pCXkQZDwS5kLAZDf6agy1GzeL71Z8lrmQzk8QQuf/1kQzxsWVlzpKNXWS7u2CJ0sN5eINMngJBfv5ZFrZgfXc86wdgUKc8aaoX8OQA1kKTcdgbE9NcAhNr1+WfNxMnz84XzmUp2Y0H1jPgGkBKQJKArfQ==',
+                                                    _secure_other_nested_setting: 'goodbye',
+                                                    insecure_nested_setting:      'dinner' }})
+
+    expect(filtered_settings._secure_setting?).to                          eql false
+    expect(filtered_settings.secure_setting?).to                           eql false
+    expect(filtered_settings._secure_other_setting).to                     eql 'hello'
+    expect(filtered_settings.secure_group._secure_nested_setting?).to      eql false
+    expect(filtered_settings.secure_group._secure_other_nested_setting).to eql 'goodbye'
+    expect(filtered_settings.secure_group.insecure_nested_setting?).to     eql false
+  end
+end
+end
+end
