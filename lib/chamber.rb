729fd14566f9d45ef5724765d3a031a0498fe150
Chris Blättermann <blaettermann@softyou.de>
2014-07-10 12:21:12 +0200

    quick fix until rubysl gets updated

--- lib/chamber.rb
+++ lib/chamber.rb
@@ -1,3 +1,11 @@
+unless Pathname.new('foo').respond_to? :write
+  class Pathname
+    def write(*args)
+      IO.write @path, *args
+    end
+  end
+end
+
 require 'chamber/instance'
 require 'chamber/rails'
 
