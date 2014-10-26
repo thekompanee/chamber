956fc60bcf0cc04c9f837d1240678c66b737ef92
Jeff Felchner <accounts+git@thekompanee.com>
2014-10-26 16:38:45 -0500

    Feature: Add a protected emailable private key when initializing
    This will allow users to be able to send their private key to their teammates
    securely.
    
    Closes #17
    
    --------------------------------------------------------------------------------
    Change-Id:     I65324c29677be39b8d8cf9e3a0a1535948fab6ca
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- lib/chamber/commands/initialize.rb
+++ lib/chamber/commands/initialize.rb
@@ -13,17 +13,45 @@ class   Initialize < Chamber::Commands::Base
   end
 
   def call
-    shell.create_file private_key_filepath, rsa_private_key.to_pem
-    shell.create_file public_key_filepath,  rsa_public_key.to_pem
+    shell.create_file private_key_filepath,    rsa_private_key.to_pem
+    shell.create_file protected_key_filepath,  rsa_protected_key
+    shell.create_file public_key_filepath,     rsa_public_key.to_pem
 
     `chmod 600 #{private_key_filepath}`
+    `chmod 600 #{protected_key_filepath}`
     `chmod 644 #{public_key_filepath}`
 
     unless ::File.read(gitignore_filepath).match(/^.chamber.pem$/)
       shell.append_to_file gitignore_filepath, private_key_filepath.basename.to_s
+      shell.append_to_file gitignore_filepath, protected_key_filepath.basename.to_s
     end
 
     shell.copy_file settings_template_filepath, settings_filepath
+
+    # rubocop:disable Metrics/LineLength
+    shell.say ''
+    shell.say 'The passphrase for your encrypted private key is:', :green
+    shell.say ''
+    shell.say rsa_key_passphrase, :green
+    shell.say ''
+    shell.say 'Store this securely somewhere.', :green
+    shell.say ''
+    shell.say 'You can send them the file located at:', :green
+    shell.say ''
+    shell.say protected_key_filepath, :green
+    shell.say ''
+    shell.say 'and not have to worry about sending it via a secure medium (such as', :green
+    shell.say 'email), however do not send the passphrase along with it.  Give it to'
+    shell.say 'your team members in person.', :green
+    shell.say ''
+    shell.say 'In order for them to decrypt it (for use with Chamber), they can run:'
+    shell.say ''
+    shell.say "$ cp /path/to/{#{protected_key_filepath.basename},#{private_key_filepath.basename}}", :green
+    shell.say "$ ssh-keygen -p -f /path/to/#{private_key_filepath.basename}", :green
+    shell.say ''
+    shell.say 'Enter the passphrase when prompted and leave the new passphrase blank.', :green
+    shell.say ''
+    # rubocop:enable Metrics/LineLength
   end
 
   def self.call(options = {})
@@ -55,6 +83,10 @@ class   Initialize < Chamber::Commands::Base
     @gitignore_filepath         ||= rootpath + '.gitignore'
   end
 
+  def protected_key_filepath
+    @protected_key_filepath     ||= rootpath + '.chamber.pem.enc'
+  end
+
   def private_key_filepath
     @private_key_filepath       ||= rootpath + '.chamber.pem'
   end
@@ -63,16 +95,25 @@ class   Initialize < Chamber::Commands::Base
     @public_key_filepath        ||= rootpath + '.chamber.pub.pem'
   end
 
-  def rsa_key
-    @rsa_key                    ||= OpenSSL::PKey::RSA.new(2048)
+  def rsa_protected_key
+    @rsa_protected_key          ||= begin
+                                      cipher = OpenSSL::Cipher.new 'AES-128-CBC'
+                                      key    = OpenSSL::PKey::RSA.new(2048)
+
+                                      key.export cipher, rsa_key_passphrase
+                                    end
   end
 
   def rsa_private_key
-    rsa_key
+    @rsa_private_key            ||= OpenSSL::PKey::RSA.new(2048)
   end
 
   def rsa_public_key
-    rsa_key.public_key
+    rsa_private_key.public_key
+  end
+
+  def rsa_key_passphrase
+    @rsa_key_passphrase         ||= SecureRandom.uuid
   end
 end
 end
