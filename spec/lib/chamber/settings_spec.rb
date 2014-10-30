3e762e55945f9b94a852e10398b9301863e12c44
Jeff Felchner <accounts+git@thekompanee.com>
2014-10-30 15:29:16 -0500

    Bugfix: Settings overridden by environment variables were decrypted
    If a setting is set in an environment variable, it should not also be decrypted
    from the YAML file.  The environment variable should take precedence.
    
    Previously, the `TranslateSecureKeysFilter` ran before the `EnvironmentFilter`,
    which meant that when the `EnvironmentFilter` got the settings, they no longer
    had the `_secure_` prefix on them.
    
    Unfortunately, when I reordered the filter list in `Settings` here: 96af235, the
    `TranslateSecureKeysFilter` came _after_ the `EnvironmentFilter`.  This caused
    the `EnvironmentFilter` to look for environment variable names such as
    `MY_GROUP__SECURE_MY_SECURE_VALUE` rather than `MY_GROUP_MY_SECURE_VALUE`.
    
    The ordering of the filters could not be reverted because the
    `FailedDecryptionFilter` needs to come after the `EnvironmentFilter` but
    _before_ the `TranslateSecureKeysFilter`.  This is due to the fact that, without
    the `_secure_` on the setting name, the `FailedDecryptionFilter` wouldn't know
    which settings it should throw errors on.
    
    The solution was simply to remove `_secure_` from keys that `Environmentable`
    was processing.
    
    --------------------------------------------------------------------------------
    Change-Id:     Ia1d24aa30b818909b31d8ce8ac87952c4fd9af10
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- spec/lib/chamber/settings_spec.rb
+++ spec/lib/chamber/settings_spec.rb
@@ -340,5 +340,40 @@ HEREDOC
     expect { settings.my_encrypted_setting }.
     to raise_error Chamber::Errors::DecryptionFailure
   end
+
+  it 'prefers environment variable values over encrypted values' do
+    ENV['MY_ENCRYPTED_SETTING']                       = 'my env setting'
+    ENV['ENCRYPTED_GROUP_MY_ENCRYPTED_GROUP_SETTING'] = 'my env group'
+
+    settings = Settings.new(
+      settings: {
+        _secure_my_encrypted_setting: 'cJbFe0NI5wknmsp2fVgpC/YeBD2pvcdVD+p0pUdnMoYTha' \
+                                      'V4mpsspg/ZTBtmjx7kMwcF6cjXFLDVw3FxptTHwzJUd4ak' \
+                                      'un6EZ57m+QzCMJYnfY95gB2/emEAQLSz4/YwsE4LDGydkE' \
+                                      'jY1ZprfXznf+rU31YGDJUTf34ESz7fsQGSc9DjkBb9ao8M' \
+                                      'v4cI7pCXkQZDwS5kLAZDf6agy1GzeL71Z8lrmQzk8QQuf/' \
+                                      '1kQzxsWVlzpKNXWS7u2CJ0sN5eINMngJBfv5ZFrZgfXc86' \
+                                      'wdgUKc8aaoX8OQA1kKTcdgbE9NcAhNr1+WfNxMnz84XzmU' \
+                                      'p2Y0H1jPgGkBKQJKArfQ==',
+        encrypted_group:              {
+          _secure_my_encrypted_group_setting: 'cJbFe0NI5wknmsp2fVgpC/YeBD2pvcdVD+p0pUdn' \
+                                              'MoYThaV4mpsspg/ZTBtmjx7kMwcF6cjXFLDVw3Fx' \
+                                              'ptTHwzJUd4akun6EZ57m+QzCMJYnfY95gB2/emEA' \
+                                              'QLSz4/YwsE4LDGydkEjY1ZprfXznf+rU31YGDJUT' \
+                                              'f34ESz7fsQGSc9DjkBb9ao8Mv4cI7pCXkQZDwS5k' \
+                                              'LAZDf6agy1GzeL71Z8lrmQzk8QQuf/1kQzxsWVlz' \
+                                              'pKNXWS7u2CJ0sN5eINMngJBfv5ZFrZgfXc86wdgU' \
+                                              'Kc8aaoX8OQA1kKTcdgbE9NcAhNr1+WfNxMnz84Xz' \
+                                              'mUp2Y0H1jPgGkBKQJKArfQ==',
+        },
+      },
+    )
+
+    expect(settings.my_encrypted_setting).to                       eql 'my env setting'
+    expect(settings.encrypted_group.my_encrypted_group_setting).to eql 'my env group'
+
+    ENV['MY_ENCRYPTED_SETTING']                       = nil
+    ENV['ENCRYPTED_GROUP_MY_ENCRYPTED_GROUP_SETTING'] = nil
+  end
 end
 end
