8aacb3f330ebcf2409172e206f81c2bf3bae9225
Jeff Felchner <accounts+git@thekompanee.com>
2016-05-11 23:16:11 -0500

    Style: Make whitespace changes

--- lib/chamber/filters/encryption_filter.rb
+++ lib/chamber/filters/encryption_filter.rb
@@ -7,8 +7,8 @@ require 'yaml'
 module    Chamber
 module    Filters
 class     EncryptionFilter
-  SECURE_KEY_TOKEN      = /\A_secure_/
-  BASE64_STRING_PATTERN = %r{\A[A-Za-z0-9\+\/]{342}==\z}
+  SECURE_KEY_TOKEN          = /\A_secure_/
+  BASE64_STRING_PATTERN     = %r{\A[A-Za-z0-9\+\/]{342}==\z}
   LARGE_DATA_STRING_PATTERN = %r{\A([A-Za-z0-9\+\/#]*\={0,2})#([A-Za-z0-9\+\/#]*\={0,2})#([A-Za-z0-9\+\/#]*\={0,2})\z}
 
   def initialize(options = {})
