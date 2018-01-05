bd354379a84021028581d3e7db238189f40a02c7
Jeff Felchner <catchall+git@jefffelchner.com>
2018-01-05 17:46:50 -0600

    Change: Configuration to force options to have been set

--- lib/chamber/configuration.rb
+++ lib/chamber/configuration.rb
@@ -13,11 +13,11 @@ class   Configuration
   def initialize(options = {})
     options              = ContextResolver.resolve(options)
 
-    self.basepath        = options[:basepath]
-    self.namespaces      = options[:namespaces]
-    self.decryption_keys = options[:decryption_keys]
-    self.encryption_keys = options[:encryption_keys]
-    self.files           = options[:files]
+    self.basepath        = options.fetch(:basepath)
+    self.namespaces      = options.fetch(:namespaces)
+    self.decryption_keys = options.fetch(:decryption_keys)
+    self.encryption_keys = options.fetch(:encryption_keys)
+    self.files           = options.fetch(:files)
   end
 
   def to_hash
