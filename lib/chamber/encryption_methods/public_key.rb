c749d39f982a6f70ac435750a7088b8009097c94
Jeff Felchner <catchall+git@jefffelchner.com>
2018-01-05 20:13:10 -0600

    Change: Singularize decryption_keys variable

--- lib/chamber/encryption_methods/public_key.rb
+++ lib/chamber/encryption_methods/public_key.rb
@@ -10,12 +10,12 @@ class   PublicKey
     Base64.strict_encode64(encrypted_string)
   end
 
-  def self.decrypt(_key, value, decryption_keys)
-    if decryption_keys.nil?
+  def self.decrypt(_key, value, decryption_key)
+    if decryption_key.nil?
       value
     else
       decoded_string    = Base64.strict_decode64(value)
-      unencrypted_value = decryption_keys.private_decrypt(decoded_string)
+      unencrypted_value = decryption_key.private_decrypt(decoded_string)
 
       begin
         _unserialized_value = YAML.load(unencrypted_value)
