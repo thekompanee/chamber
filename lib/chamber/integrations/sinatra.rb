36a45f562ce53b2b4dc494a22dd84421b7cd43d7
Jeff Felchner <catchall+git@jefffelchner.com>
2018-01-14 13:09:10 -0600

    Add: Sinatra/Padrino integration

---
+++ lib/chamber/integrations/sinatra.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'socket'
+
+module Chamber
+module Integrations
+module Sinatra
+  def self.registered(app)
+    app.configure do |inner_app|
+      env  = inner_app.environment || ENV['RACK_ENV']
+      root = inner_app.root
+
+      if defined?(Padrino)
+        env  = Padrino.env  if Padrino.respond_to?(:env)
+        root = Padrino.root if Padrino.respond_to?(:root)
+      end
+
+      Chamber.load(
+                    basepath:   root,
+                    namespaces: {
+                      environment: -> { env },
+                      hostname:    -> { Socket.gethostname },
+                    },
+                  )
+    end
+  end
+end
+end
+end
