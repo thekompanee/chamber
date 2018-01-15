5b18f20644a7f215618bc89d3fa0807a93b170e2
Jeff Felchner <catchall+git@jefffelchner.com>
2018-01-14 19:16:09 -0600

    Style: Make whitespace changes

--- lib/chamber/encryption_methods/public_key.rb
+++ lib/chamber/encryption_methods/public_key.rb
@@ -5,9 +5,9 @@ require 'base64'
 module  Chamber
 module  EncryptionMethods
 class   PublicKey
-  def self.encrypt(_key, value, encryption_keys)
-    value = YAML.dump(value)
-    encrypted_string = encryption_keys.public_encrypt(value)
+  def self.encrypt(_key, value, encryption_key)
+    value            = YAML.dump(value)
+    encrypted_string = encryption_key.public_encrypt(value)
 
     Base64.strict_encode64(encrypted_string)
   end
