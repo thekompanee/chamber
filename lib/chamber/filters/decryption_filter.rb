f49e1c9403b5283d41e2fa3a71d80343ae00588b
Carnell Washington <cwashington@ip-192-168-3-199.ec2.internal>
2019-09-16 10:26:40 -0500

    Update DecryptionFilter#decrypt to work with Ruby 2.4

--- lib/chamber/filters/decryption_filter.rb
+++ lib/chamber/filters/decryption_filter.rb
@@ -79,9 +79,11 @@ class   DecryptionFilter
     method = decryption_method(value)
 
     decryption_keys.each do |decryption_key|
-      return method.decrypt(key, value, decryption_key)
-    rescue OpenSSL::PKey::RSAError
-      next
+      begin
+        return method.decrypt(key, value, decryption_key)
+      rescue OpenSSL::PKey::RSAError
+        next
+      end
     end
 
     value
