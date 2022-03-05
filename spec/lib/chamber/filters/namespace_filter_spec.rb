f8a9d9ea0f0af96fa0ac116af5d7d30214974a3c
Jeff Felchner <catchall+git@jefffelchner.com>
2022-03-05 13:29:31 -0600

    Remove: Hashie From NamespaceFilter

--- spec/lib/chamber/filters/namespace_filter_spec.rb
+++ spec/lib/chamber/filters/namespace_filter_spec.rb
@@ -10,11 +10,11 @@ describe  NamespaceFilter do
   it 'can filter settings data based on the settings namespaces' do
     filtered_settings = NamespaceFilter.execute(
                           data:       {
-                            namespace_value:       {
-                              namespace_setting: 'value 1',
+                            'namespace_value'       => {
+                              'namespace_setting' => 'value 1',
                             },
-                            other_namespace_value: {
-                              other_namespace_setting: 'value 2',
+                            'other_namespace_value' => {
+                              'other_namespace_setting' => 'value 2',
                             },
                           },
                           namespaces: %w{namespace_value other_namespace_value},
@@ -27,11 +27,11 @@ describe  NamespaceFilter do
   it 'ignores data which is not part of a namespace' do
     filtered_settings = NamespaceFilter.execute(
                           data:       {
-                            namespace_value:      {
-                              namespace_setting: 'value 1',
+                            'namespace_value'      => {
+                              'namespace_setting' => 'value 1',
                             },
-                            non_namespaced_value: {
-                              non_namespaced_setting: 'value 2',
+                            'non_namespaced_value' => {
+                              'non_namespaced_setting' => 'value 2',
                             },
                           },
                           namespaces: %w{
@@ -46,8 +46,8 @@ describe  NamespaceFilter do
   it 'ignores namespaces which do not exist in the data' do
     filtered_settings = NamespaceFilter.execute(
                           data:       {
-                            namespace_value: {
-                              namespace_setting: 'value 1',
+                            'namespace_value' => {
+                              'namespace_setting' => 'value 1',
                             },
                           },
                           namespaces: %w{namespace_value other_namespace_value},
@@ -59,7 +59,7 @@ describe  NamespaceFilter do
   it 'does not filter data if it does not include any namespaces' do
     filtered_settings = NamespaceFilter.execute(
                           data:       {
-                            non_namespaced_setting: 'value 1',
+                            'non_namespaced_setting' => 'value 1',
                           },
                           namespaces: [],
                         )
@@ -70,15 +70,15 @@ describe  NamespaceFilter do
   it 'can filter if it is given NamespaceSets' do
     filtered_settings = NamespaceFilter.execute(
                           data:       {
-                            namespace_value:       {
-                              namespace_setting:         'value 1',
-                              another_namespace_setting: 'value 2',
+                            'namespace_value'       => {
+                              'namespace_setting'         => 'value 1',
+                              'another_namespace_setting' => 'value 2',
                             },
-                            other_namespace_value: {
-                              namespace_setting_1:         'value 1',
-                              another_namespace_setting_2: 'value 2',
+                            'other_namespace_value' => {
+                              'namespace_setting_1'         => 'value 1',
+                              'another_namespace_setting_2' => 'value 2',
                             },
-                            non_namespaced_value:  'value 3',
+                            'non_namespaced_value'  => 'value 3',
                           },
                           namespaces: NamespaceSet.new(
                                         %w{
